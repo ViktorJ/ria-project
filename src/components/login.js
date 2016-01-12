5526418aeba45eb0070c0c5c6f8cd49797cd3516
ViktorJ <rotkivnossnaj@gmail.comgit config --listgit config --global user.email rotkivnossnaj@gmail.com>
2016-01-12 13:27:19 +0100

    some refactoring

--- src/components/login.js
+++ src/components/login.js
@@ -19,35 +19,43 @@ const Login = React.createClass({
             email: this.refs.email.value,
             password: this.refs.password.value
         };
-        
+
         this.props.login(user);
-        
+
         e.preventDefault();
     },
     render: function(){
         if(this.props.auth.current === C.LOGGED_IN){
             this.redirect();
         }
-        return (
-            <div className="col-sm-6 col-md-6 col-lg-6 centered">
-            <h1 className="text-center large">YetAnotherNoteApp</h1>
-            <div className="well">
-            <h3>Login to access your notes</h3>
-            <p className="small">If you don't have an account, sign up with the button below</p>
-            <form className="form loginform" onSubmit={this.handleLoginSubmit}>
-              <div className="form-group">
-                <input type="text" className="form-control" ref="email" placeholder="Email" />
-              </div>
-              <div className="form-group">
-                <input type="password" className="form-control" ref="password" placeholder="Password" />
-              </div>
-              <button type="submit" className="btn btn-default">Login</button>
-              <button type="button" className="btn btn-info" onClick={this.props.initial}><Link to="/createUser">New account</Link></button>
-            </form>
-            <Alert alertClass={this.props.alert.alertClass} alertMsg={this.props.alert.alertMsg}/>
-            </div>
-            </div>
-        );
+        if(this.props.auth.current === C.AWAITING_AUTH){
+            return (
+                <div className="col-sm-2 col-md-2 col-lg-2 centered">
+                    <i className="fa fa-spinner fa-5x fa-spin text-center"></i>
+                </div>
+                );
+        } else {
+            return (
+                <div className="col-sm-6 col-md-6 col-lg-6 centered">
+                <h1 className="text-center large">YetAnotherNoteApp</h1>
+                <div className="well">
+                <h3>Login to access your notes</h3>
+                <p className="small">If you don't have an account, sign up with the button below</p>
+                <form className="form loginform" onSubmit={this.handleLoginSubmit}>
+                  <div className="form-group">
+                    <input type="text" className="form-control" ref="email" placeholder="Email" />
+                  </div>
+                  <div className="form-group">
+                    <input type="password" className="form-control" ref="password" placeholder="Password" />
+                  </div>
+                  <button type="submit" className="btn btn-default">Login</button>
+                  <button type="button" className="btn btn-info" onClick={this.props.initial}><Link to="/createUser">New account</Link></button>
+                </form>
+                <Alert alertClass={this.props.alert.alertClass} alertMsg={this.props.alert.alertMsg}/>
+                </div>
+                </div>
+            );
+        }
     }
 });
 
--- src/components/notes.js
+++ src/components/notes.js
@@ -12,34 +12,25 @@ const Notes = React.createClass({
         let noteArray = [];
         let key = 0;
 
-        if(this.props.auth.current === 'LOGGED_IN'){
-            if (notes) {
-                let user = this.props.auth.userId;
-                notes.forEach(function (note) {
-                    if(note.user === user){
-                        noteArray.push(<div key={key} className="well"><Link to={"/note/" + note.key} key={key++}>
-                            {note.title}
-                        </Link></div>);
-                    }
-                });
-            }
+        if (notes) {
+            let user = this.props.auth.userId;
+            notes.forEach(function (note) {
+                if(note.user === user){
+                    noteArray.push(<div key={key} className="well"><Link to={"/note/" + note.key} key={key++}>
+                        {note.title}
+                    </Link></div>);
+                }
+            });
         }
+
         return noteArray.reverse();
     },
     render: function () {
-        if(this.props.notes.data){
-            return (
-                <div className="notes">
-                    {this.renderNotes()}
-                </div>
-            );
-        } else {
-            return (
-                <div className="notes">
-                   <i className="fa fa-spinner fa-3x fa-spin"></i>
-                </div>
-                );
-        }
+        return (
+            <div className="notes">
+                {this.renderNotes()}
+            </div>
+        );
     }
 });
 
